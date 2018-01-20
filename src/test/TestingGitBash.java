20eb615996e1b8156d530d1f00f5dfbebe6f259c
Sagar <patelsagar1997@gmail.com>
2018-01-19 21:14:07 -0500

    changes from testbranch

--- src/test/TestingGitBash.java
+++ src/test/TestingGitBash.java
@@ -5,5 +5,6 @@ public class TestingGitBash {
         System.out.println("Testing Git Bash");
         System.out.println("Test #2 -- Trying to use gitignore file");
         System.out.println("Making change from GitHub");
+        System.out.println("I'm setting aside a specific branch to do these changes...");
     }
 }

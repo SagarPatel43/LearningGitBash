51212591e8edab7fb29c724f083a760dac2ca735
Sagar <patelsagar1997@gmail.com>
2018-01-19 21:05:33 -0500

    Made another change

--- src/test/TestingGitBash.java
+++ src/test/TestingGitBash.java
@@ -3,5 +3,6 @@ package test;
 public class TestingGitBash {
     public static void main(String[] args) {
         System.out.println("Testing Git Bash");
+        System.out.println("Test #2 -- Trying to use gitignore file");
     }
 }

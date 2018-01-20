04e344c2c92bf183c2cc9d0be375abd15bc83006
Sagar <patelsagar1997@gmail.com>
2018-01-19 21:16:54 -0500

    master creating merge conflict

--- src/test/TestingGitBash.java
+++ src/test/TestingGitBash.java
@@ -4,7 +4,7 @@ public class TestingGitBash {
     public static void main(String[] args) {
         System.out.println("Testing Git Bash");
         System.out.println("Test #2 -- Trying to use gitignore file");
-        System.out.println("Making change from GitHub");
+        System.out.println("Master creating merge conflict");
         System.out.println("I'm setting aside a specific branch to do these changes...");
     }
 }

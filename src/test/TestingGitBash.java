2f5f2db38f3c407f1a216432b0c04b915d7c80d7
SagarPatel43 <patelsagar1997@gmail.com>
2018-01-19 21:10:11 -0500

    changing from github

--- src/test/TestingGitBash.java
+++ src/test/TestingGitBash.java
@@ -4,5 +4,6 @@ public class TestingGitBash {
     public static void main(String[] args) {
         System.out.println("Testing Git Bash");
         System.out.println("Test #2 -- Trying to use gitignore file");
+        System.out.println("Making change from GitHub");
     }
 }
